51fd99c5151696192df0a8eb44d6ac940a9c8aff
NaoyaFurumura <hurumura1120@icloud.com>
2022-03-05 17:48:24 +0900

    test husky

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-import express from "express";
+import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("Hello World!");
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World!');
 });
+function hello(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+console.log(hello('TypeScript'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
