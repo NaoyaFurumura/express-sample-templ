cfa6b437fd97e6d8ae6de84319413bcba93c0f6c
NaoyaFurumura <hurumura1120@icloud.com>
2022-03-05 18:19:57 +0900

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['.eslintrc.*'],
+  rules: {},
+};
