3a59a7667a7e50636c0ee133c6107ef0385b8761
NaoyaFurumura <hurumura1120@icloud.com>
2022-03-06 16:32:53 +0900

    add test code

---
+++ test/sample.test.ts
@@ -0,0 +1,23 @@
+describe('test', () => {
+  async function delay(n: number) {
+    return new Promise(function (resolve) {
+      setTimeout(resolve, n * 1000);
+    });
+  }
+  test.concurrent('test', async () => {
+    console.log(1);
+    await delay(2);
+    console.log('after');
+    expect(1 + 1).toBe(2);
+  });
+
+  test.concurrent('test2', () => {
+    console.log(2);
+    expect(1 + 1).toBe(2);
+  });
+
+  test.concurrent('test', () => {
+    console.log(3);
+    expect(1 + 1).toBe(2);
+  });
+});
